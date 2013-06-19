f212140c2ad9cec819cd9866e492e29ba0104e72
FI4 OPEN SOURCE <rpcolom@gmail.com>
2013-06-19 14:45:04 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testgit::Application.config.secret_token = '02da73709f20a50e9871a0a5d936066e89d8fad817a18f709e05106cfe3b637749546b035505105db7d08b872664de96a943c57662d569014896fac002252a66'
